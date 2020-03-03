cec711fd3c45082c48f0dd0108d4a7d4dc2bfab7
tanvira <tanvira1993@gmail.com>
2020-03-03 22:26:16 +0600

    gl list view

--- views/routes.js
+++ views/routes.js
@@ -153,6 +153,23 @@ accountingApp.config(['$stateProvider', '$urlRouterProvider', function($statePro
         }
     })
 
+    .state('glList', {
+        
+        templateUrl: "layouts/glList.html",
+        controller: "GlListController",
+        resolve: {
+            deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'accountingApp',
+                    insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                    files: [
+                    "ng-asset/GlListController.js"
+                    ]
+                });
+            }]
+        }
+    })
+
     
 
 }]);
